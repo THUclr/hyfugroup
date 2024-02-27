65d712e1662fc8d8da09d78ce301e60cf4db955b
honsonchan <63185602+honsonchan@users.noreply.github.com>
2024-02-27 21:51:19 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -74,9 +74,9 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+import sphinx-rtd-theme
+html_theme = "sphinx-rtd-theme"
+html_theme_path = [sphinx-rtd-theme.get_html_theme_path()]
 html_title = "HYFUgroup Photonics Lab"
 
 # Theme options are theme-specific and customize the look and feel of a theme
