6f54d3cfc27f5f88cf2627a717c5bc42c8633562
honsonchan <63185602+honsonchan@users.noreply.github.com>
2024-12-09 18:40:24 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -64,7 +64,7 @@ language = None
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path .
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
